6b91a7f5844c4d14fb03c0fb229a6a6297273672
@eduardo <jose.eduardo@inbec.edu.br>
2024-01-30 13:02:40 -0300

    soft delete

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -8,27 +8,31 @@ class Controller {
       const listaDeRegistro = await this.entidadeService.pegaTodosOsRegistros();
       return res.status(200).json(listaDeRegistro);
     } catch (erro) {
-      // erro
+      return res.status(500).json({ erro: erro.message });
     }
   }
 
   async pegaUmPorId(req, res) {
     const { id } = req.params;
     try {
-      const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
+      const umRegistro = await this.entidadeService.pegaUmRegistroPorId(
+        Number(id)
+      );
       return res.status(200).json(umRegistro);
     } catch (erro) {
-      // erro
+      return res.status(500).json({ erro: erro.message });
     }
   }
 
   async criaNovo(req, res) {
     const dadosParaCriacao = req.body;
     try {
-      const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
+      const novoRegistroCriado = await this.entidadeService.criaRegistro(
+        dadosParaCriacao
+      );
       return res.status(200).json(novoRegistroCriado);
     } catch (erro) {
-      // erro
+      return res.status(500).json({ erro: erro.message });
     }
   }
 
@@ -37,11 +41,16 @@ class Controller {
     const dadosAtualizados = req.body;
     try {
       //isUpdated
-      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(
+        dadosAtualizados,
+        Number(id)
+      );
       if (!foiAtualizado) {
-        return res.status(400).json({ mensagem: 'registro não foi atualizado' });
+        return res
+          .status(400)
+          .json({ mensagem: "registro não foi atualizado" });
       }
-      return res.status(200).json({ mensagem: 'Atualizado com sucesso' });
+      return res.status(200).json({ mensagem: "Atualizado com sucesso" });
     } catch (erro) {
       // erro
     }
@@ -53,7 +62,7 @@ class Controller {
       await this.entidadeService.excluiRegistro(Number(id));
       return res.status(200).json({ mensagem: `id ${id} deletado` });
     } catch (error) {
-      // erro
+      return res.status(500).json({ erro: erro.message });
     }
   }
 }
