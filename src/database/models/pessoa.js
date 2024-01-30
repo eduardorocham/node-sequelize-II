da7bf0d7477d1d0fa0b3aa443e37ba9a480ca531
@eduardo <jose.eduardo@inbec.edu.br>
2024-01-30 17:44:12 -0300

    escopo de associação

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -9,8 +9,13 @@ module.exports = (sequelize, DataTypes) => {
       });
       Pessoa.hasMany(models.Matricula, {
         foreignKey: "estudante_id",
-        // scope: { status: 'matriculado' },
+        scope: { status: "matriculado" },
         as: "aulasMatriculadas",
+      });
+      Pessoa.hasMany(models.Matricula, {
+        foreignKey: "estudante_id",
+        scope: {},
+        as: "todasAsMatriculas",
       });
     }
   }
